682c6962daf7392032469e222d22c851084c5786
louevaberanger <133343765+louevaberanger@users.noreply.github.com>
2023-12-05 14:00:04 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-##hello
+#verifie les arguments 
+d1=0
+d2=0
+t=0
+s=0
+l=0
+
+for i in `seq 2 $#`
+do
+	case ${!i} in
+		"-d1") d1=1;;
+		"-d2") d2=1;;
+		"-t") t=1;;
+		"-l") l=1;;
+		"-s") s=1;;
+		"-h") echo "Options qui existent : -d1, -d2, -l, -t, -s";;
+		*) echo " ${!i} existe pas";;
+	esac
+done
+
