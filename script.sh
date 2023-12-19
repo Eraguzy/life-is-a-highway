dae9c7a3d83cd2d6dcb4aac44c9b8f7974f7c347
Lucas Gournay <133283871+Spydertnt@users.noreply.github.com>
2023-12-19 15:23:20 +0100

    Traitement d1 + gnuplot

--- script.sh
+++ script.sh
@@ -2,14 +2,13 @@
 #verifie le bon nombre d'arguments
 if [ $# -lt 2 ]
 then
-	echo "Pas le bon nombre d'arguments !"
-	exit 1
+echo "Pas le bon nombre d'arguments !"
+exit 1
 fi
 
-if [ -e "$1" ] # verifie que le chemin est bon et que le fichier étudié porte le bon nom
+if [ -e "$1" ]
 then
-    if [ "$1" == "data/data.csv" ]
-    then
+    if [ "$1" == "data/data.csv" ]; then
         echo "Le chemin est bon."
     else
         echo "Le chemin existe mais n'est pas le chemin de data.csv."
@@ -20,7 +19,7 @@ else
     exit 3
 fi
 
-#verifie les arguments : reste à 0 si l'option n'est pas activée
+#verifie les arguments
 d1=0
 d2=0
 t=0
@@ -29,79 +28,97 @@ l=0
 
 for i in `seq 2 $#`
 do
-	case ${!i} in
-		"-d1") d1=1;;
-		"-d2") d2=1;;
-		"-t") t=1;;
-		"-l") l=1;;
-		"-s") s=1;;
-		"-h") echo "Options qui existent : -d1, -d2, -l, -t, -s"
-		exit 4;; # quitte si y'a -h
-		*) echo " ${!i} existe pas";;
-	esac
+case ${!i} in
+"-d1") d1=1;;
+"-d2") d2=1;;
+"-t") t=1;;
+"-l") l=1;;
+"-s") s=1;;
+"-h") echo "Options qui existent : -d1, -d2, -l, -t, -s"
+exit 4;;
+*) echo " ${!i} existe pas";;
+esac
 done
 
-images='images'
-progc='progc'
-temp='temp'
+awk -F';' '$2 == 1' data/data.csv > etape1.csv
+#grep ";1;" data/data.csv > etape1.csv
 
-#verifie si le dossier temp existe, s'il n'existe pas ou si ce n'est pas un fichier, le dossier temp est créé
-if [ -e  "$temp" ]
-then 
-	if [ ! -d "$temp" ]
-	then
-		mkdir "$temp"
-		echo "le dossier "$temp" n'existe pas. Il vient d'être créé."	
-	else
-		echo "le dossier "$temp" existe et vient d'être vidé"
-		find "$temp" -mindepth 1 -delete
-	fi
-else 
-	mkdir "$temp"
-	echo "le dossier "$temp" n'existe pas. Il vient d'être créé."	
-fi
-
-#verifie si le dossier image existe, sinon on le crée
-if [ -e  "$images" ]
-then 
-	if [ ! -d "$images" ]
-	then
-		mkdir "$images"
-		echo "le dossier "$images" n'existe pas. Il vient d'être créé."	
-	else
-		echo "le dossier "$images" existe."	
-	fi
-else 
-	mkdir "$images"
-	echo "le dossier "$images" n'existe pas. Il vient d'être créé."	
-fi
-
-#verifie si l'executable existe, sinon il compile le fichier .c et si il n'y arrive pas il affiche un message d'erreur et quitte le programme
-if [ -e "progc/exec" ]
-then
-        echo "L'executable est présent."
-else
-        echo "L'executable n'est pas présent."
-        gcc -o exec progc/exec.c
-        if [ $? -eq 0 ] # recup la valeur de retour de gcc
-        then
-        	echo "Compilation réussie."
-        else
-        	echo "La compilation a échoué."
-        	exit 5
-        fi
-fi
+#if [ -e "exec" ]
+#then
+#        echo "L'executable est présent"
+#else
+#        echo "L'executable n'est pas présent"
+#        gcc -o exec progc/exec.c
+#        if [ $? -eq 0 ]
+#        then
+#         echo "Compilation réussie."
+#        else
+#         echo "La compilation a échoué."
+#         exit 5
+#        fi
+#fi
 
 mesurer_temps_execution() {
 
-    local start_time=$(date +%s) # temps de depart
+    # Enregistrez le moment de début
+    local start_time=$(date +%s)
 
-    $1 # Appelez la fonction passée en paramètre
+    # Appelez la fonction passée en paramètre
+    $1
 
-    local end_time=$(date +%s) # temps de fin
+    # Enregistrez le moment de fin
+    local end_time=$(date +%s)
 
-    local elapsed_time=$(echo "$end_time - $start_time" | bc) #diff entre fin et debut (bc car shell ne prend pas en compte les float de base)
+    # Calculez la différence entre le moment de fin et le moment de début en secondes
+    local elapsed_time=$(echo "$end_time - $start_time" | bc)
 
+    # Affichez la durée d'exécution en secondes avec un détail
     echo "La fonction a pris $elapsed_time secondes pour s'exécuter."
 }
 
+traitement_d1() {
+	cut -d';' -f6 etape1.csv > d1temp.csv
+	awk '{
+		# Compter les occurrences de chaque nom (sixième colonne)
+		count[$i]++
+	}
+	END {
+		#Precss
+		for (prenom in count) {
+			print prenom, ";", count[prenom]
+		}
+	}' d1temp.csv \
+	| sort -k2 -t";" -n -r \
+	| head -n 10 > d1temp2.csv
+	
+	rm d1temp.csv
+	
+	output_file="histogramme.png"
+
+	gnuplot <<-EOF
+	set terminal png size 1000,1000
+	set output "$output_file"
+	set datafile separator ";"
+	set style data histogram
+	set style fill solid
+	set boxwidth 0.5
+	set ylabel "Histogramme des chiffres associés aux prénoms"
+	set yrange [0:450]
+	
+	set xtics rotate by 90 offset 0,-11
+	set ytics rotate by 90
+	set bmargin 13
+	
+	plot "d1temp2.csv" using 2:xtic(1) with boxes notitle
+	EOF
+
+	convert -rotate 90 $output_file $output_file
+
+	xdg-open "$output_file"
+	
+}
+
+if [ $d1 -eq 1 ]
+then
+mesurer_temps_execution traitement_d1
+fi
