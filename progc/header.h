2b5a7471a9f9ed2bff4865e1d8ed2b0a3c74a8d6
Lucas Gournay <133283871+Spydertnt@users.noreply.github.com>
2024-01-03 06:18:03 +0100

    Update header.h - Avec le tri du traitement t fait

--- progc/header.h
+++ progc/header.h
@@ -11,16 +11,19 @@
         struct arbre* droite;
         int eq;
         float elmnt;
+        int elmnt2;
         char* csv; //contient toutes les autres infos de l'élément (qui ne servent pas au tri)
     }Arbre;
 
     int min(int a, int b);
     int max(int a, int b);
-    Arbre* creerarbre(float x, char* c);
-    Arbre* ajoutabr(Arbre* a, float x, char* c, int* h); //tri selon h
-    Arbre* ajoutabrchar(Arbre* a, float x, char* c, int* h); //tri selon c
+    Arbre* creerarbre(float x, int y, char* c);
+    Arbre* ajoutabr(Arbre* a, float x, int y, char* c, int* h); //tri selon h
+    Arbre* ajoutabrchar(Arbre* a, float x, int y, char* c, int* h); //tri selon c
     Arbre* equilibreravl(Arbre* a);
     void traitement_t(int argc, char* argv[]);
     void traitement_s(int argc, char* argv[]);
-    void infixepours(Arbre* arbre, FILE* fgnu, int* count);
+    void infixeinversepours(Arbre* arbre, FILE* fgnu, int* count);
+    void infixeinversepourt(Arbre* arbre, FILE* fgnu, int* count);
+    void infixepourt(Arbre* arbre, FILE* fgnu2, int* count);
 #endif
